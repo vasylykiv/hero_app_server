5cfe639a47c1cd3a4be063118b568647320e6c83
Vitaliy <vasylykivit@gmail.com>
2025-09-14 21:44:25 +0300

    010(Feat): imported controller, middlewares and utils functions into main route file

--- src/routes/R_basic.ts
+++ src/routes/R_basic.ts
@@ -1,17 +1,18 @@
 import { Router, Request, Response, NextFunction } from "express";
-import multer from "multer";
 
+import C_getData from "$controllers/C_getData";
 import C_postData from "$controllers/C_postData";
+import C_patchData from "$controllers/C_patchData";
+import C_deleteData from "$controllers/C_deleteData";
 
-import M_validateEmptyData from "$middlewares/M_validateEmptyData";
 import M_upload from "$middlewares/M_uploadFiles";
-import M_checkMultipart from "$middlewares/M_checkRequestContentType";
+import M_checkMultipart from "$middlewares/M_checkMultipart";
 
 const router = Router();
 
-router.get("", () => {});
-router.delete("", () => {});
-router.post("/add_hero", M_checkMultipart, M_upload, M_validateEmptyData, C_postData);
-router.patch("", () => {});
+router.get("/get_hero", C_getData);
+router.post("/add_hero", M_checkMultipart, M_upload, C_postData);
+router.patch("/change_hero/:id", M_checkMultipart, M_upload, C_patchData);
+router.delete("/delete_hero/:id", C_deleteData);
 
 export default router;
