277587569abdc52624df9ad9aa4cc22fd73f52db
Vitaliy <vasylykivit@gmail.com>
2025-09-14 22:05:37 +0300

    019(Feat): create delete route

---
+++ src/controllers/C_deleteData.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { PoolClient } from "pg";
+import { pool as db } from "$clientDB/D_client";
+import path from "path";
+import fs from "fs";
+
+async function C_deleteData(req: Request, res: Response, next: NextFunction) {
+  const id = req.params?.id;
+  const folderPath = path.join(process.cwd(), process.env.FILES_FOLDER, id);
+
+  if (!id) return res.status(200).json({ message: "The id parameter in link cannot be empty" });
+
+  let client: PoolClient;
+  try {
+    client = await db.connect();
+
+    const count = await client.query(
+      `
+        DELETE FROM "hero" * 
+        WHERE id = $1
+        
+    `,
+      [id]
+    );
+
+    if (count.rowCount > 0 && fs.existsSync(folderPath)) fs.rmSync(folderPath, { recursive: true, force: true });
+
+    res.status(200).json({ message: "Delete success" });
+  } catch (error) {
+    return next(error);
+  } finally {
+    client.release();
+  }
+}
+
+export default C_deleteData;
