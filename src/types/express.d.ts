3acea5ca252714942e7bcbcdfb4cf9d3d879a37d
Vitaliy <vasylykivit@gmail.com>
2025-09-16 16:53:24 +0000

    Update express.d.ts

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,14 @@
-declare namespace Express {
-  export interface Request {
-    filesFolderPath?: string;
-    deleted?: boolean;
-    created?: boolean;
-  }
-}
+export type ClientData = {
+  id?: string;
+  nickname: string;
+  real_name: string;
+  origin_description: string;
+  superpowers: string;
+  catch_phrase: string;
+};
+
+export type ClientImages = {
+  id: string;
+  hero_id: string;
+  url: string;
+};
